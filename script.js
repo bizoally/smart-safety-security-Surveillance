b2dd2a1fce5a66d70a1ed3b7c595315e793f5fda
bizoally <amhi.in01@gmail.com>
2022-01-28 10:30:30 +0530

    first commit.

---
+++ script.js
@@ -0,0 +1,21 @@
+var title1 =document.getElementById('title1');
+var text1=document.getElementById('text1');
+function changetext1(){
+
+   text1.innerHTML='COVID-19 is a disease caused by a virus called SARS-CoV-2. Most people with COVID-19 have mild symptoms, but some people can become severely ill. Although most people with COVID-19 get better within weeks of illness, some people experience post-COVID conditions. Post-COVID conditions are a wide range of new, returning, or ongoing health problems people can experience more than four weeks after first being infected with the virus that causes COVID-19. Older people and those who have certain underlying medical conditions are more likely to get severely ill from COVID-19. Vaccines against COVID-19 are safe and effective.';
+}
+function changetext2(){
+
+    text1.innerHTML='Handwashing is one of the best ways to protect yourself and your family from getting sick. Wash your hands often with soap and water for at least 20 seconds, especially after blowing your nose, coughing, or sneezing; going to the bathroom; and before eating or preparing food. If soap and water are not readily available, use an alcohol-based hand sanitizer with at least 60% alcohol.';
+ }
+ function changetext3(){
+
+    text1.innerHTML='text1';
+ }
+ function changetext4(){
+
+    text1.innerHTML='text4';
+ }
+ function back(){
+    text1.innerHTML=' <p class="showonce">Click on the Title You Want to Know about</p> ';
+ }

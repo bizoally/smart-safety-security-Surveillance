daa01d7de902b41c5c2eb53a45f604d656924fda
bizoally <93839871+bizoally@users.noreply.github.com>
2022-02-01 00:02:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var title1 =document.getElementById('title1');
 var text1=document.getElementById('text1');
 function changetext1(){
 
-   text1.innerHTML='COVID-19 is a disease caused by a virus called SARS-CoV-2. Most people with COVID-19 have mild symptoms, but some people can become severely ill. Although most people with COVID-19 get better within weeks of illness, some people experience post-COVID conditions. Post-COVID conditions are a wide range of new, returning, or ongoing health problems people can experience more than four weeks after first being infected with the virus that causes COVID-19. Older people and those who have certain underlying medical conditions are more likely to get severely ill from COVID-19. Vaccines against COVID-19 are safe and effective.';
+   text1.innerHTML='COVID-19 is a disease caused by a virus called SARS-CoV-2. Most people with COVID-19 have mild symptoms, but some people can become severely ill. Although most people with COVID-19 get better within weeks of illness, ';
 }
 function changetext2(){
 
-    text1.innerHTML='Handwashing is one of the best ways to protect yourself and your family from getting sick. Wash your hands often with soap and water for at least 20 seconds, especially after blowing your nose, coughing, or sneezing; going to the bathroom; and before eating or preparing food. If soap and water are not readily available, use an alcohol-based hand sanitizer with at least 60% alcohol.';
+    text1.innerHTML='Handwashing is one of the best ways to protect yourself and your family from getting sick. Wash your hands often with soap and water for at least 20 seconds, especially after blowing your nose, coughing, ';
  }
  function changetext3(){
 
@@ -28,4 +28,32 @@ if (mobcount==2){
    mobicon.style.display='none';
    mobcount -=2
 }
+ }
+ var faqopen=0
+ function showfaq(){
+   var faqitem=document.getElementById('faq1').style.display='block';
+   faqopen +=1;
+   if (faqopen==2){
+      var faqitem=document.getElementById('faq1').style.display='none';
+      faqopen =0
+   }
+
+ }
+ function showfaq2(){
+   var faqitem=document.getElementById('faq2').style.display='block';
+   faqopen +=1;
+   if (faqopen==2){
+      var faqitem=document.getElementById('faq2zzz').style.display='none';
+      faqopen =0
+   }
+
+ }
+ function showfaq3(){
+   var faqitem=document.getElementById('faq3').style.display='block';
+   faqopen +=1;
+   if (faqopen==2){
+      var faqitem=document.getElementById('faq3').style.display='none';
+      faqopen =0
+   }
+
  }

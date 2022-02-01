7c8bdcfaf5a5579897ca3e2de7c125ac1c7ab4a2
bizoally <93839871+bizoally@users.noreply.github.com>
2022-02-01 11:12:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ var title1 =document.getElementById('title1');
 var text1=document.getElementById('text1');
 function changetext1(){
 
-   text1.innerHTML='COVID-19 is a disease caused by a virus called SARS-CoV-2. Most people with COVID-19 have mild symptoms, but some people can become severely ill. Although most people with COVID-19 get better within weeks of illness, ';
+   text1.innerHTML='We are creating a solution of most secure Transactions Through the ATM By using photo verification ';
 }
 function changetext2(){
 
-    text1.innerHTML='Handwashing is one of the best ways to protect yourself and your family from getting sick. Wash your hands often with soap and water for at least 20 seconds, especially after blowing your nose, coughing, ';
+    text1.innerHTML='We saw many cases of fraud by using ATM and doing some tricks with the ATM or sometime by gussing pin';
  }
  function changetext3(){
 
-    text1.innerHTML='text1';
+    text1.innerHTML='ATMs Already have the software to fetch the user data based on card and also Cameras are already installed in the ATM so we can directly implement it';
  }
  function changetext4(){
 
@@ -41,20 +41,20 @@ if (mobcount==2){
  }
  var faqopen2=0
  function showfaq2(){
-   var faqitem=document.getElementById('faq2').style.display='block';
+   var faqitem2=document.getElementById('faq2').style.display='block';
    faqopen2 +=1;
    if (faqopen2==2){
-      var faqitem=document.getElementById('faq2zzz').style.display='none';
+      var faqitem2=document.getElementById('faq2').style.display='none';
       faqopen2 =0
    }
 
  }
  var faqopen3=0
  function showfaq3(){
-   var faqitem=document.getElementById('faq3').style.display='block';
+   var faqitem3=document.getElementById('faq3').style.display='block';
    faqopen3 +=1;
    if (faqopen3==2){
-      var faqitem=document.getElementById('faq3').style.display='none';
+      var faqitem3=document.getElementById('faq3').style.display='none';
       faqopen3 =0
    }
 
